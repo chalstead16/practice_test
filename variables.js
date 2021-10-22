0d3ce8537b10e73e7f04be6349beeb4da9423aff
Casey Halstead <catherineclosee@gmail.com>
2021-10-22 08:31:08 -0500

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,22 @@
+//Part II: Program Specific Challenge
+
+var famousPerson = "Hilary Duff";
+console.log(famousPerson)
+
+var isActive = true;
+console.log(isActive)
+
+famousPerson = "Ally Love";
+
+var num3 = 3;
+var num9 = 9;
+var sum = num3 + num9;
+console.log(num3)
+console.log(num9)
+console.log(sum)
+
+var animals = ["pigs", "chickens", "goats"];
+animals.shift();
+console.log(animals)
+
+console.log(`${famousPerson} ${famousPerson.length}`);
