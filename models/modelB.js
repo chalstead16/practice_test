521bf67c04ca0ede646250b2597c3d40d09a6af2
Casey Halstead <catherineclosee@gmail.com>
2021-10-22 10:24:40 -0500

    Add vehicle object model

---
+++ models/modelB.js
@@ -0,0 +1,28 @@
+//OOP - VEHICLE
+
+var make = "Jeep";
+
+var model = "Cherokee";
+
+var seats = 5;
+
+var totalPassengers = 5;
+
+var isEmpty = false;
+
+var isAllWheelDrive = true;
+
+var colorOptions = ["white", "black", "red"];
+
+var seatCoverMaterialOptions = ["leather", "neoprene", "vinyl"];
+
+var vehicle = {
+  make: "Jeep",
+  model: "Cherokee",
+  seats: 5,
+  totalPassengers: 5,
+  isEmpty: false,
+  isAllWheelDrive: true,
+  colorOptions: ["white", "black", "red"],
+  seatCoverMaterialOptions: ["leather", "neoprene", "vinyl"]
+};
