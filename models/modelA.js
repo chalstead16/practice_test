2f93791738093ae075b6962b56fe2feaaaf02e0a
Casey Halstead <catherineclosee@gmail.com>
2021-10-22 08:44:45 -0500

    Add airport object orientented programming

---
+++ models/modelA.js
@@ -0,0 +1,17 @@
+// OOP - Airport
+
+var fullName = "Kennedy International Airport";
+
+var abbreviation = "JFK";
+
+var totalTerminals = 9;
+
+var totalPassengers = 625e5;
+
+var isOpen = true;
+
+var isBusy = true;
+
+var carries = ["American Airlines", "Delta Air Lines", "JetBlue"];
+
+var directFlights = ["Los Angeles, CA", "Miami, FL", "Dallas, TX","Chicago, IL"];
